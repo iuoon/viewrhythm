969dacdb1b765c995e1644a37cac5759a69ca1b7
iuoon <1272461442@qq.com>
2019-08-26 23:01:18 +0800

    代码提交

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -13,6 +13,6 @@ export default {
 <style scoped>
 .head{
   height: 65px;
-  border-bottom: 1px solid #2d8cf0;
+  border-bottom: 1px solid #57a3f3d9;
 }
 </style>
---
+++ src/components/Left.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="left_1">
+      <Collapse class="collapse_c_1" simple >
+        <Panel name="1" class="panel_c_1">
+          史蒂夫·乔布斯
+          <p slot="content" >史蒂夫·乔布斯（Steve Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。</p>
+        </Panel>
+        <Panel name="2">
+          斯蒂夫·盖瑞·沃兹尼亚克
+          <p slot="content">斯蒂夫·盖瑞·沃兹尼亚克（Stephen Gary Wozniak），美国电脑工程师，曾与史蒂夫·乔布斯合伙创立苹果电脑（今之苹果公司）。斯蒂夫·盖瑞·沃兹尼亚克曾就读于美国科罗拉多大学，后转学入美国著名高等学府加州大学伯克利分校（UC Berkeley）并获得电机工程及计算机（EECS）本科学位（1987年）。</p>
+        </Panel>
+        <Panel name="3">
+          乔纳森·伊夫
+          <p slot="content">乔纳森·伊夫是一位工业设计师，现任Apple公司设计师兼资深副总裁，英国爵士。他曾参与设计了iPod，iMac，iPhone，iPad等众多苹果产品。除了乔布斯，他是对苹果那些著名的产品最有影响力的人。</p>
+        </Panel>
+      </Collapse>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Left'
+}
+</script>
+
+<style scoped>
+  .left_1{
+    width: 18%;
+    border-right: 1px solid #57a3f3d9;
+    height: 100%;
+  }
+  .ivu-collapse-header{
+    height: 38px;
+    line-height: 38px;
+    padding-left: 16px;
+    color: #2d8cf0 !important;
+    cursor: pointer;
+    position: relative;
+    border-bottom: 1px solid transparent;
+    transition: all .2s ease-in-out;
+  }
+  .collapse_c_1{
+    float: left;
+    width: 90%;
+    text-align: left;
+    /*box-shadow: 0px 0px 11px 0px #b6b9ff;*/
+    margin-top: 20px;
+  }
+
+</style>
