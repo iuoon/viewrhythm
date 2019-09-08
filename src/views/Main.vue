0f78d624161fa6a1b31a8d63b0a50af7a77c4890
iuoon <1272461442@qq.com>
2019-09-08 22:44:35 +0800

    代码提交

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <div id="div1" @drop="drop" @dragover="allowDrop" style="{width:90%; height:200px; margin:10px;padding:10px;border:1px solid #aaaaaa;}">
-      <img src="../assets/logo.png" draggable="true" @dragstart="drag" id="drag1" />
+      <img src="../assets/logo.png" draggable="true" @dragstart="drag" id="drag1" style="position: relative;float:left;left:20px;top:10px;" />
     </div>
     <div id="div2" @drop="drop" @dragover="allowDrop"  style="{width:90%; height:200px; margin:10px;padding:10px;border:1px solid #aaaaaa;}"></div>
   </div>
@@ -13,15 +13,16 @@ export default {
   name: 'Main',
   methods: {
     allowDrop (ev) {
+      console.log('11', ev)
       ev.preventDefault()
     },
     drag (ev) {
-      console.log(ev)
+      console.log('00', ev)
       ev.dataTransfer.setData('Text', ev.target.id)
     },
 
     drop (ev) {
-      console.log(ev)
+      console.log('22', ev)
       ev.preventDefault()
       const data = ev.dataTransfer.getData('Text')
       ev.target.appendChild(document.getElementById(data))
