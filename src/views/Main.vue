bfa628a436c664dae04ccd4c5f2088ab284ad478
iuoon <1272461442@qq.com>
2019-09-08 23:59:40 +0800

    代码提交

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <div id="div1" @drop="drop" @dragover="allowDrop" style="{width:90%; height:200px; margin:10px;padding:10px;border:1px solid #aaaaaa;}">
-      <img src="../assets/logo.png" draggable="true" @dragstart="drag" id="drag1" :style="elcss" />
+      <img src="../assets/logo.png" draggable="true" @dragstart="dragstart" id="drag1" :style="elc" />
     </div>
     <div id="div2" @drop="drop" @dragover="allowDrop"  style="{width:90%; height:200px; margin:10px;padding:10px;border:1px solid #aaaaaa;}"></div>
   </div>
@@ -13,24 +13,34 @@ export default {
   name: 'Main',
   data () {
     return {
-      elcss: {
+      elc: {
         position: 'relative', float: 'left', left: '20px', top: '10px'
       },
-      elpos: {
-        x: 0, y: 0
+      elp: {
+        offsetX: 0,
+        offsetY: 0,
+        x: 0,
+        y: 0
       }
     }
   },
   methods: {
     allowDrop (e) {
       e.preventDefault()
-      this.elpos.x = e.offsetX || e.layerX
-      this.elpos.y = e.offsetY || e.layerY
-      console.log(this.elpos.x, this.elpos.y)
+      this.elp.x = e.pageX
+      this.elp.y = e.pageY
+      if (this.elp.x === 0 && this.elp.y === 0) {
+        return false
+      }
+      this.elp.x -= this.elp.offsetX
+      this.elp.y -= this.elp.offsetY
+      console.log(this.elp.x, this.elp.y)
     },
-    drag (e) {
+    dragstart (e) {
       console.log('00', e)
       e.dataTransfer.setData('Text', e.target.id)
+      this.elp.offsetX = e.offsetX
+      this.elp.offsetY = e.offsetY
     },
 
     drop (e) {
