f27b6ec23ddc476eeab7c97483166a541a4d91d2
iuoon <mhwuyz@163.com>
2019-09-16 16:23:28 +0800

    代码修改

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Index from './views/Index.vue'
+import Common from './views/Common.vue'
 
 Vue.use(Router)
 
@@ -26,6 +27,11 @@ export default new Router({
       path: '/index',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/common',
+      name: 'common',
+      component: Common
     }
   ]
 })
---
+++ src/views/Common.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+ <div @mοusemοve="move1(this)" @mousedown=drag($event) style="position: relative; width:98%; height:200px;width: 200px; margin:10px;padding:10px;border:1px solid #aaaaaa;"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Common',
+  methods: {
+    move1 (e) {
+      console.log(e)
+    },
+    drag (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
